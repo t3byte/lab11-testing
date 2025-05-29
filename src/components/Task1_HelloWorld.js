c93f662be9c41bfab9c81917078d002efaaf8f65
t3byte <125903242+t3byte@users.noreply.github.com>
2025-05-30 00:13:55 +0500

    Update Task1_HelloWorld.js

--- src/components/Task1_HelloWorld.js
+++ src/components/Task1_HelloWorld.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Task1_HelloWorld = () => {
-  return <h1 data-testid="hello-heading">Hello world</h1>;
+  return <h1 data-testid="hello-heading">Hello world on another branch</h1>;
 };
 
 export default Task1_HelloWorld;
